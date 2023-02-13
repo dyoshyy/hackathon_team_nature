0abc664c7f683349a6dd411d60cd0c42b8091b1d
dyoshy <d.yoshhyy@gmail.com>
2023-02-13 11:08:36 +0900

    'loginadd'

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Request
+from flask import Flask, render_template, Request, request
 import sqlite3
 app = Flask(__name__)
 
@@ -12,6 +12,15 @@ def index():
     conn.close()
     return render_template('index.html', list = list)
 
+@app.route('/login_top')
+def login_top():
+    return render_template('login.html')
+
+@app.route('/login',methods=["POST"])
+def login():
+    name = request.form['name']
+    password = request.form['pass']
+    return render_template('login_result.html', name = name, password = password)
 
 
 if __name__ == "__main__":
